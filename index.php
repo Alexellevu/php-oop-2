1b138ca0098f73b24e1b6031d832dea7ccda9e1f
alexellevu <newnapster83@gmail.com>
2021-06-28 16:31:13 +0200

    create le classi user e product

--- index.php
+++ index.php
@@ -2,9 +2,44 @@
 
 
 class user {
+   private $name;
+   private $lastname;
+   private $mail;
+   private $password;
+   private $type;
+
+   function __construct($name, $lastname, $mail, $password, $type)
+   {
+    $this ->name= $name;
+    $this ->lastname = $lastname;
+    $this ->mail= $mail;
+    $this ->password =$password;
+    $this->type = $type;
     
+   }
+
 
 }
+
+class product 
+{
+    public $name;
+    public $brand;
+    public $quanty;
+    public $price;
+    
+   function __construct($name, $brand, $quanty, $price)
+   {
+    $this ->name= $name;
+    $this ->brand = $brand;
+    $this ->quanty= $quanty;
+    $this ->price =$price;
+    
+    
+   }
+    
+}
+
 ?>
 
 
